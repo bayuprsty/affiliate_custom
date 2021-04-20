c09bc0c30d7517589b16be2df40b7842c53becf9
bayuprsty <bayuprsty24@gmail.com>
2021-04-20 10:25:21 +0700

    - Change track js

--- public/track.js
+++ public/track.js
@@ -1,7 +1,7 @@
 function sc(url) {
     if (url.search("leadid") > 0) {
         console.log(url);
-        var urlSplit = url.split('?');
+        var urlSplit = url.split('&');
         var data = urlSplit[1];
 
         document.cookie = data;
